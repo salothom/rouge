304aeacff95b9d232e12668f6b00c8798df32e6e
Sarah Thompson <sarahloisthompson@gmail.com>
2020-05-11 21:25:55 -0500

    more

--- constants.py
+++ constants.py
@@ -1,15 +1,15 @@
-import pygame
+# import pygame
 
-pygame.init()
+# pygame.init()
 
-game_width = 800
-game_height = 600
+# game_width = 800
+# game_height = 600
 
-#definition
-COLOR_BLACK = (0,0,0)
-COLOR_WHITE = (255,255,255)
-COLOR_GREY = (100,100,100)
-#color
-COLOR_DEFAULT_BG = COLOR_GREY
-#sprit
-S_PLAYER = pygame.image.load("data/me.png")
+# #definition
+# COLOR_BLACK = (0,0,0)
+# COLOR_WHITE = (255,255,255)
+# COLOR_GREY = (100,100,100)
+# #color
+# COLOR_DEFAULT_BG = COLOR_GREY
+# #sprit
+# S_PLAYER = pygame.image.load("data/me.png")
